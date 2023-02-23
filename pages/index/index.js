3635ae3b4110b4f3384fdc866703177fb4d7b2f6
Shuobin-P <3120465015@qq.com>
2023-02-23 22:01:14 +0800

    add function: iterate cardList And render data on index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,19 @@ Page({
   data: {
     active: 1,
     number: 22,
-    imageURL: "https://img95.699pic.com/photo/50075/6447.jpg_wh300.jpg"
-
+    imageURL: "https://img95.699pic.com/photo/50075/6447.jpg_wh300.jpg",
+    cardList: [
+        {
+            title: "测试1",
+            originPrice: 100,
+            price: 20,
+        },
+        {
+            title: "测试2",
+            originPrice: 200,
+            price: 50,
+        }
+    ]
   },
   // 事件处理函数
   bindViewTap() {
