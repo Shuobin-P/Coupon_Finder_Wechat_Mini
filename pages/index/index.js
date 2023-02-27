9bdafb206155b934547beeace6ad6d83bf0b94e3
Shuobin-P <3120465015@qq.com>
2023-02-27 22:07:54 +0800

    FIXME:can't render data correctly when switching between food and drink tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,13 +5,11 @@ const app = getApp()
 Page({
     data: {
         activeTab: 1,
-        number: 22,
         imageURL: "https://img95.699pic.com/photo/50075/6447.jpg_wh300.jpg",
-        cardList: [{
-            title: "默认初始数据",
-            originPrice: 100,
-            price: 20,
-        }]
+        pageNum: 1,
+        pageSize: 10,
+        hasMore: true, // 是否还有更多数据
+        cardList: []
     },
     onChange(event) {
         this.data.activeTab = event.detail.index;
@@ -25,8 +23,19 @@ Page({
                 }],
                 activeTab: 0
             })
-        } else if (this.data.activeTab === 1) {
+
+        }
+        //下面这里好像activeTab赋值好像有问题。
+        else if (this.data.activeTab === 1) {
+            //数据为切换到美食tab，再切换回来，cardList中的数据要清空
+            this.setData({
+                cardList: [],
+            })
+            //FIXME: 这里不能直接调用这个，因为每次调用pageNum就会加1
+            //只要改页面没有被销毁，每次调用getDrinkData()就会导致
+            //pageNum加1
             this.getDrinkData();
+            
         }
     },
     // 事件处理函数
@@ -38,29 +47,53 @@ Page({
     /*从微信进入第一次进入该页面时，这个函数会先被调用，那么
     页面需要展示的数据，也是从这里向后台拿 */
     onLoad() {
-        this.getDrinkData();
+        //如果调用this.this.getDrinkData();
+        //显示的数据会出现重复
     },
     /*从别的页面进入该页面时，更新页面数据 */
     onShow() {
-        this.getDrinkData();
+        //onLoad和onShow如果都调用getDrinkData则会出现数据重复的问题
     },
     getDrinkData() {
+        if (!this.data.hasMore) {
+            return
+        }
+        wx.showLoading({
+            title: '加载中...',
+        })
+
         let _this = this;
         wx.request({
             url: 'http://localhost:8080/coupon/getHotCoupons', // 后台 API 地址
+            data: {
+                pageNum: this.data.pageNum,
+                pageSize: this.data.pageSize,
+            },
             header: {
                 'content-type': 'application/json'
             },
             success(res) {
+                wx.hideLoading();
+                const data = res.data.data || []
+                const newDataList = _this.data.cardList.concat(data)
                 _this.setData({
-                    //这个res有点问题，这里把cardList的值写死的话，就没啥问题
-                    ["cardList"]: res.data.data
+                    ["cardList"]: newDataList,
+                    ["hasMore"]: data.length >= _this.data.pageSize,
+                    pageNum: _this.data.pageNum + 1,
                 })
             },
             fail(err) {
-                console.error(err);
+                wx.hideLoading();
+                wx.showToast({
+                    title: '加载失败，请重试',
+                    icon: 'none',
+                })
             }
         });
     },
-
+    // 上滑触底事件
+    onReachBottom() {
+        // 加载下一页数据
+        this.getDrinkData()
+    },
 })
